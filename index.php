2fb594f8550e86454f389fb40c545e61988a7dd8
Taixing Lin <olinlinlino@gmail.com>
2014-10-16 00:54:11 +0800

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("head.html")
 ?>
 <div style="position: relative;">
-<img src="img/MainBackgroundback2.jpg" alt="" class="bg">
+<img src="img/MainBackgroundback2.jpg" alt="" class="bg" style="width:100%;max-width:100%">
 <img class="igem1 animated fadeInUpBig" src="img/1.png" style="width:26%;position: absolute; left:3.7em; bottom:0;">
 <img class="igem2 animated fadeInUpBig" src="img/2.png" style="width:53%;position: absolute; left:26%; bottom:0;">
 <img class="igem3 animated fadeInUpBig" src="img/3.png" style="width:28%;position: absolute; right:3.7em; bottom:0;">
