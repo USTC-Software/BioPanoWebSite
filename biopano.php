cb1971e975a4c89aa6061bbdf2fe26693be941a0
Taixing Lin <olinlinlino@gmail.com>
2014-10-16 04:28:29 +0800

    test

--- biopano.php
+++ biopano.php
@@ -2,8 +2,8 @@
 require_once("head.html")
 ?>
 <div class="main-content">
-     <div class="row large-12 columns large-centered">
-        <p style="font-size:32px">Biopano</p>
+     <div class="row large-12 columns large-centered wiki-head">
+        <p style="font-size:64px;height:100%;line-height:100%;color:rgb(144,144,144)">Meet Biopano</p>
         
       </div>
       <div class="row">
@@ -18,7 +18,22 @@ require_once("head.html")
       <h4 id="m_b">Motivation & Background</h4>
       <div class="underline-small"></div>
         <p>
-          《科学美国人》(Scientific American)杂志编辑比艾罗(David Biello)曾经用过一个简单的 比喻,来说明什么是合成生物学:如果将生命比作电脑,那么,由许多核酸组成的程式码—— 基因体,就是生命的作业系统(operating system)。 合成生物学想做的就是,透过创造或改写基因体,让生命表现出预期的行为,执行预定的工 作。然而,有时候我们会把生命的程式写“坏”了,就像你把电脑的作业系统弄坏了一样;电脑 会因此开不了机,而生命机器也会因此不正常或是死亡。 通过尝试错误(trial and error)的方式,累积成功与失败的经验,人们就会渐渐了解生命程式 的规则与语法,进而掌握撰写生命蓝图的法则。 现有的设计应用存在于生物体中的调控模式,去不断地组合、套用,产生更为丰富的人工生物 学机器,应用到了我们生活的各个角落。然而,仅仅应用已发现的调控模式,利用标准化元件 去“拼凑”合成生物学机器远远不能满足我们理想。 正如万有引力定律支配着漫天繁星的运转,我们也有理由相信在生物机器里有许多未知的规律 在支配着生命的运行。 梳理已知的生命体内物质间的联系、分析网络中暗含的调控模式、总结支配生命运转的规律, 这些帮助我们理解生命本质的工作或许会为合成生物学的发展起到更加本质上的推动作用。每 一个新的调控模式的发现,都会极大地拓展合成生物学研究人员的设计手段,产生更加精巧的 设计思路。 同时,我们也注意到分子生物学的研究日趋细化,绝大多数的学者的研究领域仅限于生物体具 体的某一部分,需要有一种机制去整合每一点成果,从一个更高的角度去理解每一部分。
+          As it is known to all, synthetic biology has achieved remarkable progresses recently. However, it is still facing great challenges.Synthetic biologist Chris Voigt once injected the toggle switch
+successfully works in bacillus subtilis to colibacillus. Nevertheless, it 
+failed to work as expected. In experiment, since the BioBrick interacts 
+with the host environment unexpectedly, it is common that the designed 
+circuit doesn’t work as anticipated.In fact, biological system is involved. Lots of biological parts interact
+with each other in complex ways to support the system to function 
+well. Many biologists try to simplify the model by finding the 
+important regulation modes. However, a myriad of data in various 
+databases makes combing the connection of materials difficult, 
+thus divergent thinking seems even tougher.Meanwhile, people realize that research focus on merely one thing
+cannot explain all biological phenomenon. Connection between c-
+di-GMP Signaling and Quorum Sensing plays a significant role in 
+transporting information in many cells, the study of which may 
+bring our research more inspiration, even enhance the 
+development of the cross biological areas.This is where our primary thought of the software comes from. We
+hope to solve the problems above lucidly by data visualization.
         </p>
     
       <h4 id="features">Features</h4>
@@ -57,21 +72,38 @@ require_once("head.html")
       <div class="underline-small">
       </div>
       <div>
-        <h4>下面我们将操作展示E.coli k-12几个常见的Module:</h4>
+        <h4>Next we will demonstrate some common modules of E.coli k-12:</h4>
         <li>
-          <h5>1,Lac操纵子：当你输入乳糖操纵子的一个Transcription Unit(TU) lacZYA，并对这个点进行Expand</h5>
-          <img src="img/A.png">
-          <p>你会看到在lacZYA这条TU内的上游序列有lacZp1,lacZp2,lacZp3,lacZp4四个启动子，下游有lacZ,lacY,lacA三个结构基因。同时，这个TU在结构上属于lacZYA这个Operon.</p>
-          <p>在lacZYA结点上方，你会看到还有CRP，H-NS和LacI三个Transcription Factor对lacZYA的表达产生抑制作用。</p>
-          <p>再分别展开lacZ,lacY,lacA三个gene会得到编码蛋白BgaL,LacY,ThgA.其中Bgal和ThgA是Enzyme,它们催化相应的反应。展开LacI得到编码它的gene lacI.</p>
-          <img src="img/B.png">
-          <img src="img/C.png">
-          <p>再展开LacZYA Operon和lacI gene会发现更多的物质和它们之间的联系。你可以选中并拖动多个节点(黄色圆圈包围)，使网络的排布更加整齐好看；也可以选中下方Full Screen全屏展示。</p>
-          <img src="img/D.png">
-          <img src="img/E.png">
+          <h5>1, Lac Operon: When you input LacZYA, a Transcription Unit(TU) of Lac Operon and Expand the node:</h5>
+          <p>You will see four promoters, acZp1, lacZp2, lacZp3 and lacZp4 as
+upstream sequences and three structure genes, lacZ, lacY and lacA as 
+downstream sequences. Meanwhile, you can see the TU belongs to 
+the Operon lacZYA.
+         </p>
+          <p>Above the node lacZYA, you also see three Transcription
+Factors, namely, CRP, H-NS and lacl. They suppress the expression of 
+lazZYA.</p>
+          <p>Expand lacZ, lacY and lacA, and we get BgaL, LacY and ThgA
+respectively, of which Bgal and ThgA are Enzymes. They catalyze
+corresponding reactions. Expand Lacl and we get the gene lacl which
+codes it.</p>
+       <ul class="small-block-grid-2">
+          <li><img src="img/B.png"></li>
+          <li><img src="img/C.png"></li>
+          </ul>
+        
+          <p>Continue to Expand lacZYA and lacl and we find more materials’
+connected with them. You can select and drag the nodes to arrange the 
+network neatly; you can also click Full Screen and the network will be 
+displayed in full screen.</p>
+          <ul class="small-block-grid-2">
+          <li><img src="img/D.png"></li>
+          <li><img src="img/E.png"></li>
+          </ul>
         </li>
         <li>
-          <h5>2,同理按照上面的步骤我们也可以得到Trp操纵子的调控代谢网络。</h5>
+          <h5>2, Following the same steps, we can also get the regulation network of
+the Operon Trp.</h5>
           <img src="img/F.png">
         </li>
 
@@ -82,17 +114,22 @@ require_once("head.html")
       <div class="underline-small"></div>
       <div>
        <p> Biopano ,do the experiment as you do in the lab.</p>
-       <p>合成生物学家在实验中经常发现，biobrick组成的基因线路与宿主细胞不兼容，导致不能正常工作。而Biopano用序列比对的方式，不仅为您分析实验失败的可能原因，而且为您设计出更精巧的基因线路提供参考方向。 </p>
-       <p>我们的blast算法来自NCBI，但是它只有序列比对功能，却不能帮您分析导入的gene circuit可能受到宿主哪些影响，更无法预测它是否能正常工作。</p>
-       <p>首先Biopano提供biobrick查询小助手，让您便捷的了解igem官网所有标准化原件。其次您还可以输入一个biobrick甚至任意构造一段全新的序列，软件利用权威的blast算法为您精确找出E.coli K-12相似性最高的基因结点，再Expand这些点找出这个基因可能受到哪些调控。这样Biopano可以帮你分析导入的gene circuit是否可能正常工作。</p>
-       <p>Biopano以数据可视化的形式，将各种生物元件以及作用关系抽象成点和边，通过不断对结点的Expand动态展示E.coli K-12基因调控和代谢网络。我们后端整合了Regulon DB,KEGG和Uniprot大量的数据，让你可以在物质的海洋中不断Expand,Expand，梳理出它们的各种联系并满足你探索的好奇心。</p>
-       <p>相比而言，cytoscape和Netscience主要支持文件导入展示网络，没有后端强大的数据库支持，所以无法在线搜索生物元件，也无法按照个人需求展示局部生物网络。</p>
-       <p>既然生命体中有那么多美妙的物质联系，越来越多的生物学家希望找到两种看似不相干的生物元件的具体作用路径，甚至两个生物模块，如c-di-GMP signalling和Quorum Sensing。但是一条条作用关系只是以记录形式存储在数据库中，你很难从数以万计的记录中挖掘出一条基因线路，之前也没有生物软件实现这样的功能，而Biopano带来了希望。</p>
-       <p>你可以任意输入两个生物元件，设定最短的几条路径，便可以找到它们之间的联系。而我们也凭借它这一强大功能找到了c-di-GMP signalling和Quorum Sensing具体联系，并且在论文中也得到了证实，这是多么惊喜的事！</p>
-       <p>当然，Biopano帮您找到的基因线路不一定是正确的，但是为您探索两个领域的联系给出了参考方向，不仅让您在设计试验时目标更加明确，而且让您从全局的视角理解生命体内联系如此紧密。</p>
-       <p>We have to admit that Biopano may show you wrong gene route. However, it provides you with direction of studying the relationship of two fields. It not only makes your goal for experiment designing more clear, but enables you to understand the connection inside body is so close.</p>
-       <p>Biopano在提供各种功能服务的同时，也作为一个合作建网平台为大家服务。您可以自定义新的节点和边，登陆google和百度账号将自己搭建的网络同步到数据库。你也可以邀请多个合作伙伴共同完成一个project。而常见的生物软件cytoscape和Netscience却不能提供一个良好的交流平台，Biopano以数据共享的形式加强了生物多领域的交流。</p>
-       <p>Biopano,生物研究者的得力助手，你值得拥有！</p>
+       <p>In experiment, synthetic biologists always find gene circuits formed by BioBricks are incompatible with the host cell, thus cannot work as expected. However, Biopano uses sequence alignment to analyze the possible reasons of the failure and design more acute gene circuit for your consideration.</p>
+       <p>Our Blast algorism derives from NCBI. However, the original version only offers sequence alignment and fails to analyze what impact the host environment might have on the injected gene circuit, let along predicts whether it will work.</p>
+       <p>Firstly, Biopano offers BioBrick searching to make it convenient for you to study all standardized biological parts on the official website of iGEM. Secondly, you can input a BioBrick and even construct a brand new sequence. Biopano will find the gene nodes of E.coli K-12 sharing the deepest homology with it based on authoritative Blast algorithm. Then you can Expand these nodes to find out the possible regulation on them. In this way, Biopano helps you analyze whether the injected gene circuit will actually work.</p>
+       <p>Biopano abstracts biological parts and the regulation among them into nodes and arcs to realize data visualization, and displays gene regulation and metabolism network of E.coli K-12 dynamically by continually Expanding nodes. Single database cannot make an entire material network, so our back end combines Regulon DB, KEGG, Uniprot and abundant data. You can ceaselessly Expand the nodes in the network to comb the relationship among them and satisfy your curiosity.</p>
+       
+       <p>Compared with Biopano, Cytoscape and Netscience mainly support file importing and network displaying without powerful database from the back end. Hence you cannot search for biological parts online and view the certain part of the biological network you need with them.</p>
+       
+       <p>Now that there is enormous astonishing material relationship in biological body, more and more biologists hope to find the specific function route between two biological parts or even two biological blocks that seem not related to each other, such as c-di-GMP signalling and Quorum Sensing. Unfortunately, the interactions of the biological parts are saved in database as records and it is almost impossible to find the route in thousands of records. There haven’t been any software to offer any help before. Luckily, Biopano brings hope to this.</p>
+      
+       <p>You can input two biological parts and set several shortest routes, then you will find their relationship. We used this powerful function to find the specific relationship betweem c-di-GMP signalling and Quorum Sensing, which was corroborated by corresponding literature. How amazing!</p>
+       
+       <p>We have to admit that Biopano may show you wrong gene route. However, it provides you with direction of studying the relationship of two fields. It not only makes your goal for experiment designing clearer, but enables you to understand the connection inside body is so close as well.</p>
+       
+       <p>While offering function services, Biopano serves as a platform for collaborative network building. You can define new nodes and arcs yourself and sign in with your Google or Baidu account to upload the network you build to our database. You can also invite several companions to complete a project together. Common biological software, such as Cytoscape and Netscience, doesn’t provide a good platform for communication, while Biopano enhances the communication of biological areas by data sharing.</p>
+      
+       <p>Biopano, your right hand for biological research. Get it now!</p>
       </div>
 
        <h4>Future Work</h4>
