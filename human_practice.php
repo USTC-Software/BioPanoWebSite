99b5b57e08716d46622bf391455f5beef7b69b81
Xue Yuechuan <xueyuechuan@gmail.com>
2014-10-16 23:34:41 +0800

    change human practice

--- human_practice.php
+++ human_practice.php
@@ -7,37 +7,79 @@ require_once("head.html")
         
       </div>
       <div class="row large-12 columns large-centered">
-        <h3>Managementssss</h3>
+        <h3>Management<br />
+            ——IGEM Efficiency Environment Enjoyment
+        </h3>
+        <div class="underline-small">
+        
+      </div>
+      <p>During the entire iGEM project, we adhered to the four elements, iGEM, efficiency, environment and enjoyment. When the project is drawing to a close, we tease out our project management experience according to them and put it online for reference to more people.</p>
      <div class="row large-12 columns large-centered">
-       <h4>Management</h4>
+       <h4>iGEM</h4>
       <div class="underline-small">
         
       </div>
-        <p>当然同时，将文件存储在云端也是一种极安全的存储方式，以防你万一把咖啡
-泼在了电脑或稿纸上。</p>
+        <p>As our project is for participating in IGEM, we always emphasized the leading position of IGEM in our project. We set up study group specifically for IGEM project. Independent of the working group, they worked on synthetic biology. Although some of their results turned out to be useless, they played a critical role in every phase of our project, such as the decision of our final project, the refinement of functions of the software and teaching other team members not majoring in biology some biological knowledge. A group focusing on the subject guarantees that the whole project runs smoothly on track. The experience above applies to project of any subject. Just replace “IGEM” with your own word.</p>
 
-        <h4>Biology Cooperation</h4>
+        <h4>Efficiency</h4>
+      <div class="underline-small">
+        
+      </div>
+        <p>In any project, efficiency is always a significant factor. During our project, we took regular and necessary methods, such as wholesome team management system (team leader, team manager, group supervisor) and explicit regulations. In addition, we managed our team in an innovative way—electronic platform. All work plans are put on electronic notebook shared by all team members. We also built a platform for team members to report their working achievements periodically. Last but not least, we established a gitlab ourselves accessible to all team members, which avoided the detriment of environment caused by paper-based platform (this will be mentioned again later).</p>
+
+        <h4>Environment</h4>
       <div class="underline-small">
         
       </div>
         <p>
-- Background（⽣物学家的交流⽅式，研究领域分⽴，影响）<br />
-- 软件合作功能描述（配图，配案例）<br />
-- 展望<br />
-- 意义（增进联系，类⽐wikipedia，成果分享）<br />
-- ⽤户的使⽤体验<br /></p>
-
-        <h4> iGEM Meetup</h4>
+ Environmental protection is one of the most compelling problems nowadays. We kept thinking about this issue while proceeding the project. Firstly, as mentioned above, we built a paperless office, except for necessary invoices. Compared with previous IGEM teams of USTC, we saved a lot of paper. Meanwhile, we paid attention to keeping our working environment neat and clean. The workplace was cleaned by our team members every day. Garbage was classified and dropped in the garbage bin. We believe that appropriate regulation can ensure that working group of any project can do their work comfortably and protect the earth at the same time.
+        </p>
+<h4>Enjoyment</h4>
       <div class="underline-small">
         
       </div>
         <p>
- - 组委会提供的Meetup说明<br />
-- 活动过程记录、在校学⽣旁听<br />
-- 成果、合⼯⼤<br />
-- 各队反馈<br />
+ Although there were a lot of work to do, our project experience was joyful. At the beginning of the project, team members with experience in software development prepared courses for other members. Well-prepared courses made us accomplish the assignments and gain new knowledge. We also organized various kinds of activities to relax our team members. In the summer vacation, we had group activities after a whole day’s hard work, including having dinner outside, playing sports and even computer game competition. We also recommend these activities to every team. They not only relax team members, but also improve the relationship among team members, making the whole working experience enjoyable and unforgettable.
+
         </p>
   </div>
+  <div class="row large-12 columns large-centered">
+        <h3>Examples with softwares</h3>
+        <div class="underline-small">
+        
+      </div>
+     <div class="row large-12 columns large-centered">
+       <h4>Google group</h4>
+      <div class="underline-small">
+        
+      </div>
+        <p>At the formation of USTC-Software of 2014, the team leader of 2013 recommended Google group as an online platform for communication and team management. Although every team member was highly enthusiastic about iGEM, they had their own school work to worry about. Meanwhile, our team members lived in different areas of the campus. Hence it became a serious problem that how team members could receive notices in time to accomplish the work separately while co-working time could not be guaranteed. Google group solved this problem greatly. Google group offers open online platform to users and users can build their own platform for sharing. Messages on the platform are accessible to all users joining the group. Emails can also be pushed to users. We built a platform of our team, and all messages on the platform could be pushed to team members in time. By Google group, we were able to:
+           <br />
+        </p>
+
+        <h4>Efficiency</h4>
+      <div class="underline-small">
+        
+      </div>
+        <p>In any project, efficiency is always a significant factor. During our project, we took regular and necessary methods, such as wholesome team management system (team leader, team manager, group supervisor) and explicit regulations. In addition, we managed our team in an innovative way—electronic platform. All work plans are put on electronic notebook shared by all team members. We also built a platform for team members to report their working achievements periodically. Last but not least, we established a gitlab ourselves accessible to all team members, which avoided the detriment of environment caused by paper-based platform (this will be mentioned again later).</p>
+
+        <h4>Environment</h4>
+      <div class="underline-small">
+        
+      </div>
+        <p>
+ Environmental protection is one of the most compelling problems nowadays. We kept thinking about this issue while proceeding the project. Firstly, as mentioned above, we built a paperless office, except for necessary invoices. Compared with previous IGEM teams of USTC, we saved a lot of paper. Meanwhile, we paid attention to keeping our working environment neat and clean. The workplace was cleaned by our team members every day. Garbage was classified and dropped in the garbage bin. We believe that appropriate regulation can ensure that working group of any project can do their work comfortably and protect the earth at the same time.
+        </p>
+<h4>Enjoyment</h4>
+      <div class="underline-small">
+        
+      </div>
+        <p>
+ Although there were a lot of work to do, our project experience was joyful. At the beginning of the project, team members with experience in software development prepared courses for other members. Well-prepared courses made us accomplish the assignments and gain new knowledge. We also organized various kinds of activities to relax our team members. In the summer vacation, we had group activities after a whole day’s hard work, including having dinner outside, playing sports and even computer game competition. We also recommend these activities to every team. They not only relax team members, but also improve the relationship among team members, making the whole working experience enjoyable and unforgettable.
+
+        </p>
+  </div>
+
   </div>
 
 </div>
