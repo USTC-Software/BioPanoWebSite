ef33be5c51c9f3642cfd55d84701ecb983b33cba
Xue Yuechuan <xueyuechuan@gmail.com>
2014-10-16 22:36:55 +0800

    add team member fyc and a few fixes

--- team.php
+++ team.php
@@ -110,7 +110,7 @@ top: 0;
               </div>
          </li>
           <li class ="profilewrap">
-              <img src="img/ltx2.jpg" alt="Haiyan Liu">
+              <img src="img/ltx.jpg" alt="Haiyan Liu">
               <div class ="nameplate"><a href="#">Lin Taixing</a></div>
               <div class = "details"><img style="width:160px" src="img/ltx.jpg" alt="Haiyan Liu"  align="left"><p>Haiyan Liu was born in Sichuan Province, China. He received his BS degree in Biology in 1990 and PhD degree in Biochemistry and Molecular Biology in 1996, both from USTC. Between 1993 and 1995 he was a visiting graduate student in Laboratory of Physical Chemistry of ETH, Zurich (Switzerland).  Since 2001, he has been a professor of computational biology at School of Life Sciences, USTC. </p>
               </div>
@@ -137,14 +137,14 @@ top: 0;
  		<li class ="profilewrap">
               <img src="img/dcx.jpg" alt="Haiyan Liu">
               <div class ="nameplate"><a href="#">Dong Chenxiao</a></div>
-              <div class = "details"><img style="width:160px" src="img/dcx.jpg" alt="Haiyan Liu"  align="left"><p>Haiyan Liu was born in Sichuan Province, China. He received his BS degree in Biology in 1990 and PhD degree in Biochemistry and Molecular Biology in 1996, both from USTC. Between 1993 and 1995 he was a visiting graduate student in Laboratory of Physical Chemistry of ETH, Zurich (Switzerland).  Since 2001, he has been a professor of computational biology at School of Life Sciences, USTC. </p>
+              <div class = "details"><img style="width:160px" src="img/dcx.jpg" alt="Haiyan Liu"  align="left"><p>All the time, try killing any idea from team.</p>
               </div>
         </li>
         
         <li class ="profilewrap">
               <img src="img/jzy.jpg" alt="Haiyan Liu">
               <div class ="nameplate"><a href="#">Jin Zeyu</a></div>
-              <div class = "details"><img style="width:160px" src="img/jzy.jpg" alt="Haiyan Liu"  align="left"><p>Haiyan Liu was born in Sichuan Province, China. He received his BS degree in Biology in 1990 and PhD degree in Biochemistry and Molecular Biology in 1996, both from USTC. Between 1993 and 1995 he was a visiting graduate student in Laboratory of Physical Chemistry of ETH, Zurich (Switzerland).  Since 2001, he has been a professor of computational biology at School of Life Sciences, USTC. </p>
+              <div class = "details"><img style="width:160px" src="img/jzy.jpg" alt="Haiyan Liu"  align="left"><p>Not most powerful, but highly responsible!</p>
               </div>
         </li>
         
@@ -158,13 +158,21 @@ top: 0;
          <li class ="profilewrap">
               <img src="img/hlx.jpg" alt="Haiyan Liu">
               <div class ="nameplate"><a href="#">Han Luxin</a></div>
-              <div class = "details"><img style="width:160px" src="img/hlx.jpg" alt="Haiyan Liu"  align="left"><p>Where there is a will, there is a way. </p>
+              <div class = "details"><img style="width:160px" src="img/hlx.jpg" alt="Haiyan Liu"  align="left"><p>Where there is a will, there is a way.</p>
               </div>
         </li>
                <li class ="profilewrap">
               <img src="img/zhy.jpg" alt="Haiyan Liu">
               <div class ="nameplate"><a href="#">Zhang Renying</a></div>
               <div class = "details"><img style="width:160px" src="img/zhy.jpg" alt="Haiyan Liu"  align="left"><p>Be the best yourself, to be No.1. </p>
+              </div>
+        </li>
+        
+        </li>
+               <li class ="profilewrap">
+              <img src="img/fyc.jpg" alt="Haiyan Liu">
+              <div class ="nameplate"><a href="#">Fei Yicheng</a></div>
+              <div class = "details"><img style="width:160px" src="img/fyc.jpg" alt="Haiyan Liu"  align="left"><p>Be the best yourself, to be No.1. </p>
               </div>
         </li>
         
