e73c159f10c4f34c1728dfc7c74cc0999e79a0dd
Xue Yuechuan <xueyuechuan@gmail.com>
2014-10-16 21:27:43 +0800

    add team member update 9

--- team.php
+++ team.php
@@ -139,7 +139,12 @@ top: 0;
               <div class = "details"><img style="width:160px" src="img/gyf.jpg" alt="Haiyan Liu"  align="left"><p>Haiyan Liu was born in Sichuan Province, China. He received his BS degree in Biology in 1990 and PhD degree in Biochemistry and Molecular Biology in 1996, both from USTC. Between 1993 and 1995 he was a visiting graduate student in Laboratory of Physical Chemistry of ETH, Zurich (Switzerland).  Since 2001, he has been a professor of computational biology at School of Life Sciences, USTC. </p>
               </div>
          </li>
-
+ 		<li class ="profilewrap">
+              <img src="img/dcx.jpg" alt="Haiyan Liu">
+              <div class ="nameplate"><a href="#">Dong Chenxiao</a></div>
+              <div class = "details"><img style="width:160px" src="img/dcx.jpg" alt="Haiyan Liu"  align="left"><p>Haiyan Liu was born in Sichuan Province, China. He received his BS degree in Biology in 1990 and PhD degree in Biochemistry and Molecular Biology in 1996, both from USTC. Between 1993 and 1995 he was a visiting graduate student in Laboratory of Physical Chemistry of ETH, Zurich (Switzerland).  Since 2001, he has been a professor of computational biology at School of Life Sciences, USTC. </p>
+              </div>
+         </li>
 
 
 
