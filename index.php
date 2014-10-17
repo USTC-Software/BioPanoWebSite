ab3c22650281ddf65dcba2db6b2b5bce0d6e217a
Xue Yuechuan <xueyuechuan@gmail.com>
2014-10-18 04:44:46 +0800

    change 1

--- index.php
+++ index.php
@@ -36,14 +36,16 @@ require_once("head.html")
 	<img src="img/page5.jpg" alt="" class="bg" style="width:100%;max-width:100%">
 </div>
 
+
+
+<div style="position: relative;">
+	<img src="img/page7.jpg" alt="" class="bg" style="width:100%;max-width:100%">
+</div>
+
 <div style="position: relative;" class="text-center ">
 	<div  class="button" style="margin: 1em 0;color:#fff;font-size:3em;background-color: #C8A0A0;">
 		Download Now !
 	</div>
-</div>
-
-<div style="position: relative;">
-	<img src="img/page7.jpg" alt="" class="bg" style="width:100%;max-width:100%">
 </div>
 
 <div class="clear"></div>
