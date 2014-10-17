e3794c89f31a7cc9205b9ffaf8c7aba0b519779e
Xue Yuechuan <xueyuechuan@gmail.com>
2014-10-17 12:52:49 +0800

    add page 1-6

--- index.php
+++ index.php
@@ -9,49 +9,32 @@ require_once("head.html")
 
 
 <div style="position: relative;">
-<img src="img/MainBackgroundback2.jpg" alt="" class="bg" style="width:100%;max-width:100%">
-<!-- 
-<img class="igem1 animated fadeInUpBig" src="img/1.png" style="width:26%;position: absolute; left:3.7em; bottom:0;">
-<img class="igem2 animated fadeInUpBig" src="img/2.png" style="width:53%;position: absolute; left:26%; bottom:0;">
-<img class="igem3 animated fadeInUpBig" src="img/3.png" style="width:28%;position: absolute; right:3.7em; bottom:0;">
- -->
+	<img src="img/MainBackgroundback2.jpg" alt="" class="bg" style="width:100%;max-width:100%">
 </div>
 
- <div style="position: relative;">
-<img src="img/page1.jpg" alt="" class="bg" style="width:100%;max-width:100%">
-<!-- 
-<img class="igem1 animated fadeInUpBig" src="img/1.png" style="width:26%;position: absolute; left:3.7em; bottom:0;">
-<img class="igem2 animated fadeInUpBig" src="img/2.png" style="width:53%;position: absolute; left:26%; bottom:0;">
-<img class="igem3 animated fadeInUpBig" src="img/3.png" style="width:28%;position: absolute; right:3.7em; bottom:0;">
- -->
-</div>
-<!--
-<section id="features-hero">
-
-<div class="row">
-<div class="small-12 columns">
-
-</div>
-</div>
-</section>
--->
-<!--
-<section id="features-hero">
-<div class="row">
-<div class="small-12 columns">
-  
-<img src="img/b1.jpg" alt="" style="width:126px;height:126px;">
-
-<span style="font-size: 4.5rem;color: rgb(38,64,84);font-family: 'Roboto Condensed', sans-serif;padding-left: 11px;position: relative;top: 26px;">BioPano 3</span>
-
-<h2 style="color:rgb(94,94,94);font-family: 'Noto Sans', sans-serif;margin-top:20px;font-weight: normal;">Draw a biological panorama for you.</h2>
-<h4 style="color:rgb(58,58,58);font-family: 'Noto Sans', sans-serif;">Specifically for biological research, multifunctional cooperative software platform aimed at better visualisation of biological relationships.</h4>
-
-</div>
+<div style="position: relative;">
+	<img src="img/page1.jpg" alt="" class="bg" style="width:100%;max-width:100%">
 </div>
 
-</section>
--->
+<div style="position: relative;">
+	<img src="img/page2.jpg" alt="" class="bg" style="width:100%;max-width:100%">
+</div>
+
+<div style="position: relative;">
+	<img src="img/page3.jpg" alt="" class="bg" style="width:100%;max-width:100%">
+</div>
+
+<div style="position: relative;">
+	<img src="img/page4.jpg" alt="" class="bg" style="width:100%;max-width:100%">
+</div>
+
+<div style="position: relative;">
+	<img src="img/page5.jpg" alt="" class="bg" style="width:100%;max-width:100%">
+</div>
+
+<div style="position: relative;">
+	<img src="img/page6.jpg" alt="" class="bg" style="width:100%;max-width:100%">
+</div>
 
 <div class="clear"></div>
       
