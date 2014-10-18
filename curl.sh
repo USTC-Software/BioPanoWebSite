2ba9856dc2e9794c74aee67191816cfa14af80e6
Taixing Lin <olinlinlino@gmail.com>
2014-10-18 10:27:01 +0800

    etst

--- curl.sh
+++ curl.sh
@@ -8,6 +8,7 @@ curl 127.0.0.1:1234/requirements.php > html/4-requirements.html
 curl 127.0.0.1:1234/human_practice.php > html/5-human_practice.html
 curl 127.0.0.1:1234/team.php > html/6-team.html
 curl 127.0.0.1:1234/acknowledge.php > html/7-acknowledge.html
+curl 127.0.0.1:1234/server.php > html/8-server.html
 mv head.html head1.html && mv head.html.bak head.html
 mv foot.html foot1.html && mv foot.html.bak foot.html
 
